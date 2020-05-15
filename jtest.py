b6de791ccda19ac567aa3fb889b781ff796a3b84
jdj2261 <jdj2261@github.com>
2020-05-15 18:13:29 +0900

    modify jtest.py

--- jtest.py
+++ jtest.py
@@ -1,5 +1,10 @@
 import os, struct, array
+import serial
+import time
+
 from fcntl import ioctl
+
+ser = serial.Serial('/dev/serial0',9600)
 
 # /dev/input에 있는 js로 시작되는 장치들이 있다면 화면에 출력
 # js0, js1 ... 게임패드나 조이스틱이 이해 해당된다.
@@ -46,46 +51,88 @@ axis_names = {
 
 # 버튼의 이름을 알기 위한
 # 키 맵핑을 위해 사용
-button_names = {
-    0x120 : 'trigger',
-    0x121 : 'thumb',
-    0x122 : 'thumb2',
-    0x123 : 'top',
-    0x124 : 'top2',
-    0x125 : 'pinkie',
-    0x126 : 'base',
-    0x127 : 'base2',
-    0x128 : 'base3',
-    0x129 : 'base4',
-    0x12a : 'base5',
-    0x12b : 'base6',
-    0x12f : 'dead',
-    0x130 : 'a',
-    0x131 : 'b',
-    0x132 : 'c',
-    0x133 : 'x',
-    0x134 : 'y',
-    0x135 : 'z',
-    0x136 : 'tl',
-    0x137 : 'tr',
-    0x138 : 'tl2',
-    0x139 : 'tr2',
-    0x13a : 'select',
-    0x13b : 'start',
-    0x13c : 'mode',
-    0x13d : 'thumbl',
-    0x13e : 'thumbr',
+# button_names = {
+#     0x120 : 'trigger',
+#     0x121 : 'thumb',
+#     0x122 : 'thumb2',
+#     0x123 : 'top',
+#     0x124 : 'top2',
+#     0x125 : 'pinkie',
+#     0x126 : 'base',
+#     0x127 : 'base2',
+#     0x128 : 'base3',
+#     0x129 : 'base4',
+#     0x12a : 'base5',
+#     0x12b : 'base6',
+#     0x12f : 'dead',
+#     0x130 : 'a',
+#     0x131 : 'b',
+#     0x132 : 'c',
+#     0x133 : 'x',
+#     0x134 : 'y',
+#     0x135 : 'z',
+#     0x136 : 'tl',
+#     0x137 : 'tr',
+#     0x138 : 'tl2',
+#     0x139 : 'tr2',
+#     0x13a : 'select',
+#     0x13b : 'start',
+#     0x13c : 'mode',
+#     0x13d : 'thumbl',
+#     0x13e : 'thumbr',
 
-    0x220 : 'dpad_up',
-    0x221 : 'dpad_down',
-    0x222 : 'dpad_left',
-    0x223 : 'dpad_right',
+#     0x220 : 'dpad_up',
+#     0x221 : 'dpad_down',
+#     0x222 : 'dpad_left',
+#     0x223 : 'dpad_right',
+
+#     # XBox 360 controller uses these codes.
+#     0x2c0 : 'dpad_left',
+#     0x2c1 : 'dpad_right',
+#     0x2c2 : 'dpad_up',
+#     0x2c3 : 'dpad_down',
+# }
+
+button_names = {
+    0x120 : '0x120',
+    0x121 : '0x121',
+    0x122 : '0x122',
+    0x123 : '0x123',
+    0x124 : '0x124',
+    0x125 : '0x125',
+    0x126 : '0x126',
+    0x127 : '0x127',
+    0x128 : '0x128',
+    0x129 : '0x129',
+    0x12a : '0x12a',
+    0x12b : '0x12b',
+    0x12f : '0x12f',
+    0x130 : '0x130',
+    0x131 : '0x131',
+    0x132 : '0x132',
+    0x133 : '0x133',
+    0x134 : '0x134',
+    0x135 : '0x135',
+    0x136 : '0x136',
+    0x137 : '0x137',
+    0x138 : '0x138',
+    0x139 : '0x139',
+    0x13a : '0x13a',
+    0x13b : '0x13b',
+    0x13c : '0x13c',
+    0x13d : '0x13d',
+    0x13e : '0x13e',
+
+    0x220 : '0x220',
+    0x221 : '0x221',
+    0x222 : '0x222',
+    0x223 : '0x223',
 
     # XBox 360 controller uses these codes.
-    0x2c0 : 'dpad_left',
-    0x2c1 : 'dpad_right',
-    0x2c2 : 'dpad_up',
-    0x2c3 : 'dpad_down',
+    0x2c0 : '0x2c0',
+    0x2c1 : '0x2c1',
+    0x2c2 : '0x2c2',
+    0x2c3 : '0x2c3',
 }
 
 axis_map = []
@@ -170,10 +217,22 @@ while True:
             if button:      
                 # 해당 버튼 상태(button_states)에 value 값으로 변경          
                 button_states[button] = value
-                if value:
-                    print("%s pressed" % (button))
-                else:
-                    print("%s released" % (button))
+                # if value:
+                #     print("%s pressed" % (button))
+                # else:
+                #     print("%s released" % (button))
+
+                
+                send_button_data = str(number)
+                send_button_value_data = str(value)
+                send_data = send_button_data + send_button_value_data
+                ser.write(send_data.encode('utf-8'))
+                #ser.write(send_button_data.encode('utf-8'))
+                #ser.write(send_button_value_data.encode('utf-8'))
+
+                # print("---test--- {0} \n".format(button_names[]))
+
+                print("{0} - {1} : \t{2}".format(send_button_data, button, send_button_value_data))
 
         # type이 0x02이면 축이 이동한 상태이다.
         if type & 0x02:
@@ -186,4 +245,40 @@ while True:
                 fvalue = value / 32767.0
                 # 상태값(0, 1, -1)을 저장
                 axis_states[axis] = fvalue
-                print("%s: %.3f" % (axis, fvalue))
+
+
+                send_axis_data = str(number)
+                send_axis_value_data = str(axis_states[axis])
+                ser.write(send_axis_data.encode('utf-8'))
+                ser.write(send_axis_value_data.encode('utf-8'))
+
+                print("{0} - {1} : \t{2}".format(send_axis_data, axis, send_axis_value_data))
+
+    
+        # send_data = '2'
+
+        # ser.write(send_data.encode('utf-8'))
+        #ser.write(bytes(send_data.encode()))
+        
+        # send_data = bytearray([0x01,0x02])
+        # ser.write(bytes(send_data))
+        
+        
+        
+
+    # try:
+    # # ser.write(bytes(bytearray([0x01,0x02])))
+    # # response = ser.readline()
+    # # print(str(response))
+    #     send_data = '2'
+    #     ser.write(bytes(send_data.encode()))
+        
+    #     # send_data = bytearray([0x01,0x02])
+    #     # ser.write(bytes(send_data))
+        
+    #     print("send Msg : {0}".format(send_data))
+        
+    #     # time.sleep(1)
+    # except KeyboardInterrupt:
+    #     ser.close()
+    #     print("ctrl + c pressed..")
